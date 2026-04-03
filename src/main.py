14f434be927168fe4e01c0b4029136f1a0a5593b
abdosameh <abdosamehdev@gmail.com>
2026-04-03 11:34:47 +0200

    setup Motor and DB

--- src/main.py
+++ src/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 from routes import base_router, data_router
+from motor.motor_asyncio import AsyncIOMotorClient
+from contextlib import asynccontextmanager
+from helpers import get_settings
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup app
+    settings = get_settings()
+    app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
+    app.db_client = app.mongo_conn[settings.MONGODB_DB_NAME]
+    yield
+    # shutdown app
+    app.mongo_conn.close()
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(base_router)
 app.include_router(data_router)
