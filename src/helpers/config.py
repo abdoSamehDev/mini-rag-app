7aeb406c78eae5a42800690aa65766d53c7c92d1
abdosameh <abdosamehdev@gmail.com>
2026-04-03 11:35:47 +0200

    edit config to have DB ulr, and name

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -4,10 +4,15 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
+
     OPENAI_API_KEY: str
+
     FILE_ALLOWED_TYPES: list[str]
     FILE_MAX_SIZE: int
     FILE_DEFAULT_CHUNK_SIZE: int
+
+    MONGODB_URL: str
+    MONGODB_DB_NAME: str
 
     class Config:
         env_file = ".env"
