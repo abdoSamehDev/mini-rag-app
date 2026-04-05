004c812051869617645ee0b28b5cf26360030cd3
abdosameh <abdosamehdev@gmail.com>
2026-04-05 16:20:39 +0200

    update main to call llm providers on startup

--- src/main.py
+++ src/main.py
@@ -3,20 +3,46 @@ from routes import base_router, data_router
 from motor.motor_asyncio import AsyncIOMotorClient
 from contextlib import asynccontextmanager
 from helpers import get_settings
+from stores import LLMProviderFactory
+
+
+async def startup_span(app: FastAPI):
+    settings = get_settings()
+
+    app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
+    app.db_client = app.mongo_conn[settings.MONGODB_DB_NAME]
+
+    llm_provider_factory = LLMProviderFactory(config=settings)
+
+    # generation client
+    app.generation_client = llm_provider_factory.create(
+        provider=settings.GENERATION_BACKEDND
+    )
+    app.generation_client.set_generation_model(model_id=settings.GENERATION_MODEL_ID)
+    # embedding client
+    app.embedding_client = llm_provider_factory.create(
+        provider=settings.EMBEDDING_BACKEDND
+    )
+    app.embedding_client.set_embedding_model(
+        model_id=settings.EMBEDDING_MODEL_ID, model_size=settings.EMBEDDING_MODEL_SIZE
+    )
+
+
+async def shutdown_span(app: FastAPI):
+    app.mongo_conn.close()
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # startup app
-    settings = get_settings()
-    app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
-    app.db_client = app.mongo_conn[settings.MONGODB_DB_NAME]
+    startup_span(app)
     # shutdown app
     yield
-    app.mongo_conn.close()
+    shutdown_span(app)
 
 
 app = FastAPI(lifespan=lifespan)
+
 
 app.include_router(base_router)
 app.include_router(data_router)
