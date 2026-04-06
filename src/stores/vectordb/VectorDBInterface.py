65f5e07efe9892fa4ee0b983626bffbdde79d1c6
abdosameh <abdosamehdev@gmail.com>
2026-04-06 21:53:18 +0200

    added vector db interface

---
+++ src/stores/vectordb/VectorDBInterface.py
@@ -0,0 +1,66 @@
+from abc import ABC, abstractmethod
+from qdrant_client import models
+
+
+class VectorDBInterface(ABC):
+    @abstractmethod
+    def connect(self):
+        pass
+
+    @abstractmethod
+    def disconnect(self):
+        pass
+
+    @abstractmethod
+    def is_collection_exists(self, collection_name: str) -> bool:
+        pass
+
+    @abstractmethod
+    def list_all_collections(self) -> models.CollectionsResponse:
+        pass
+
+    @abstractmethod
+    def get_collection_info(self, collection_name: str) -> models.CollectionInfo:
+        pass
+
+    @abstractmethod
+    def delete_collection(self, collection_name: str) -> bool:
+        pass
+
+    @abstractmethod
+    def create_collection(
+        self,
+        collection_name: str,
+        embedding_size: int,
+        do_reset: bool = False,
+    ) -> bool:
+        pass
+
+    @abstractmethod
+    def insert_one(
+        self,
+        collection_name: str,
+        text: str,
+        vector: list,
+        metadata: dict,
+        record_id: str = None,
+    ) -> bool | None:
+        pass
+
+    @abstractmethod
+    def insert_many(
+        self,
+        collection_name: str,
+        texts: list[str],
+        vectors: list,
+        metadatas: list[dict] = None,
+        record_ids: list[str] = None,
+        batch_size: int = 50,
+    ) -> bool | None:
+        pass
+
+    @abstractmethod
+    def search_by_vector(
+        self, collection_name: str, vector: list, limit: int = 5
+    ) -> list[dict] | None:
+        pass
