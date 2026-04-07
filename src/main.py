35b9e7a3227b7281c360eef732bb425d4d09590f
abdosameh <abdosamehdev@gmail.com>
2026-04-07 18:39:38 +0200

    update main with vector db factory

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from routes import base_router, data_router
 from motor.motor_asyncio import AsyncIOMotorClient
 from contextlib import asynccontextmanager
 from helpers import get_settings
-from stores import LLMProviderFactory
+from stores import LLMProviderFactory, VectorDBProviderFactory
 
 
 async def startup_span(app: FastAPI):
@@ -13,6 +13,7 @@ async def startup_span(app: FastAPI):
     app.db_client = app.mongo_conn[settings.MONGODB_DB_NAME]
 
     llm_provider_factory = LLMProviderFactory(config=settings)
+    vector_db_provider_factory = VectorDBProviderFactory(config=settings)
 
     # generation client
     app.generation_client = llm_provider_factory.create(
@@ -26,10 +27,15 @@ async def startup_span(app: FastAPI):
     app.embedding_client.set_embedding_model(
         model_id=settings.EMBEDDING_MODEL_ID, model_size=settings.EMBEDDING_MODEL_SIZE
     )
+    # vector db client
+    app.vector_db_client = vector_db_provider_factory.create(settings.VECTOR_DB_BACKEND)
+    app.vector_db_client.connect()
 
 
 async def shutdown_span(app: FastAPI):
     app.mongo_conn.close()
+    # vector db client
+    app.vector_db_client.disconnect()
 
 
 @asynccontextmanager
