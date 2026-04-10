2bfcfcb026fcf735ecbaaa1d8915e7aada978316
abdosameh <abdosamehdev@gmail.com>
2026-04-10 04:28:23 +0200

    update data controller

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,7 +1,7 @@
 from .BaseController import BaseController
 from .ProjectController import ProjectController
 from fastapi import UploadFile
-from models import ResponseMessage
+from models import ResponseMessageEnums
 import re
 import os
 
@@ -24,14 +24,14 @@ class DataController(BaseController):
 
         if file and file.size is not None:
             if file.content_type not in self.app_settings.FILE_ALLOWED_TYPES:
-                return False, ResponseMessage.FILE_TYPE_NOT_SUPPORTED.value
+                return False, ResponseMessageEnums.FILE_TYPE_NOT_SUPPORTED.value
 
             if file.size > self.app_settings.FILE_MAX_SIZE * self.size_scale:
-                return False, ResponseMessage.FILE_SIZE_EXCEEDED.value
+                return False, ResponseMessageEnums.FILE_SIZE_EXCEEDED.value
 
-            return True, ResponseMessage.FILE_UPLOAD_SUCCESS.value
+            return True, ResponseMessageEnums.FILE_UPLOAD_SUCCESS.value
         else:
-            return False, ResponseMessage.FILE_NOT_EXIST.value
+            return False, ResponseMessageEnums.FILE_NOT_EXIST.value
 
     def generate_unique_file_path(self, orignal_file_name: str, project_id: str):
         random_key = self.generate_random_string()
