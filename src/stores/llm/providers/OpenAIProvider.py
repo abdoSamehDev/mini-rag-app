44abeede512a6fd5d86066f8fa56c225907dfcb4
abdosameh <abdosamehdev@gmail.com>
2026-04-12 17:25:07 +0200

    some updates for OpenAIProvider to run with the Local LLM (Ollama)

--- src/stores/llm/providers/OpenAIProvider.py
+++ src/stores/llm/providers/OpenAIProvider.py
@@ -74,18 +74,37 @@ class OpenAIProvider(LLMInterface):
                 self.construct_prompt(prompt, OpenAIEnums.USER.value)
             ]
 
-        response = self.client.responses.create(
+        # FOR THE REMOTE MODELS APIs
+        # response = self.client.responses.create(
+        #     model=self.generation_model_id,
+        #     input=messages,
+        #     max_output_tokens=max_output_tokens,
+        #     temperature=temperature,
+        # )
+
+        # FOR OLLAMA WITH OPENAI (LOCAL MODELS)
+        response = self.client.chat.completions.create(
             model=self.generation_model_id,
-            input=messages,
-            max_output_tokens=max_output_tokens,
+            messages=messages,
+            # max_completion_tokens=max_output_tokens,
             temperature=temperature,
         )
 
         # validate the response and its output
-        if not response or not response.output_text:
+        if (
+            not response
+            or not response.choices
+            or len(response.choices) == 0
+            or not response.choices[0].message
+            or not response.choices[0].message.content
+        ):
             self.logger.error("Error while generating text with OpenAI API.")
             return None
-        return response.output_text
+        # # FOR THE REMOTE MODELS APIs
+        # return response.output_text
+
+        # FOR OLLAMA WITH OPENAI (LOCAL MODELS)
+        return response.choices[0].message.content
 
         # chat_history.append(self.construct_prompt(prompt, OpenAIRolesEnum.USER.value))
 
