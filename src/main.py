0e71fdefdf2069467a3afa3fa785064e783ec20b
abdosameh <abdosamehdev@gmail.com>
2026-04-18 20:54:51 +0200

    added pg session to main

--- src/main.py
+++ src/main.py
@@ -4,13 +4,22 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from contextlib import asynccontextmanager
 from helpers import get_settings
 from stores import LLMProviderFactory, VectorDBProviderFactory, TemplateParser
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
 
 
 async def startup_span(app: FastAPI):
     settings = get_settings()
 
     app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
-    app.db_client = app.mongo_conn[settings.MONGODB_DB_NAME]
+    # app.db_client = app.mongo_conn[settings.MONGODB_DB_NAME]
+
+    postgres_conn = f"postgresql+asyncpg://{settings.POSTGRES_USERNAME}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_MAIN_DATABASE}"
+
+    app.db_engine = create_async_engine(postgres_conn)
+    app.db_client = sessionmaker(
+        app.db_engine, class_=AsyncSession, expire_on_commit=False
+    )
 
     llm_provider_factory = LLMProviderFactory(config=settings)
     vector_db_provider_factory = VectorDBProviderFactory(config=settings)
@@ -42,6 +51,8 @@ async def shutdown_span(app: FastAPI):
     app.mongo_conn.close()
     # vector db client
     app.vector_db_client.disconnect()
+    # postgres
+    await app.db_engine.dispose()
 
 
 @asynccontextmanager
