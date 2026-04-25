2f06f528768c6f4bf4cbfaa4210184c081d06bf3
abdosameh <abdosamehdev@gmail.com>
2026-04-25 11:45:59 +0300

    update app settings

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -26,6 +26,8 @@ class Settings(BaseSettings):
     OPENAI_API_KEY: str = None
     OPENAI_API_URL: str = None
     COHERE_API_KEY: str = None
+    OLLAMA_API_KEY: str = None
+    OLLAMA_API_URL: str = None
 
     GENERATION_MODEL_ID: str = None
     EMBEDDING_MODEL_ID: str = None
@@ -35,9 +37,11 @@ class Settings(BaseSettings):
     GENERATION_DEFAULT_MAX_TOKENS: int = None
     GENERATION_DEFAULT_TEMPERATURE: float = None
 
+    VECTOR_DB_BACKEND_LITERAL: list[str] = None
     VECTOR_DB_BACKEND: str
     VECTOR_DB_PATH: str
     VECTOR_DB_DISTANCE_METHOD: str = None
+    VECTOR_DB_PGVEC_INDEX_THRESHOLD: int = 100
 
     PRIMARY_LANG: str = "en"
     DEFAULT_LANG: str = "en"
