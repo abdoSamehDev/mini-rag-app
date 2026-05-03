c1bc74ba0c96b316937ff3cb7f1d21bce05ada36
abdosameh <abdosamehdev@gmail.com>
2026-05-03 21:41:34 +0300

    reinitiate alembic for dockerizing

--- src/models/db_schemes/__init__.py
+++ src/models/db_schemes/__init__.py
@@ -1,7 +1,7 @@
 from .asset_db_scheme import Asset as Asset
 from .chunk_db_scheme import Chunk as Chunk, RetrievedDocument as RetrievedDocument
 from .project_db_scheme import Project as Project
-from .mini_rag import (
+from .minirag import (
     PgProject as PgProject,
     PgAsset as PgAsset,
     PgChunk as PgChunk,
